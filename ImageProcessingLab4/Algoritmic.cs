a584efa2eb97718a625d3be09a7e7dec302474e6
Zainutdinov Vladislav <vladzainytdinov06@gmail.com>
2025-11-21 16:35:43 +0400

    Сохранение

---
+++ ImageProcessingLab4/Algoritmic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageProcessingLab4
+{
+    public static class Algoritmic
+    {
+        public static BitmapSource ConvertRGBToGray(BitmapSource source)
+        {
+            WriteableBitmap writeableBitmap = new WriteableBitmap(source);
+
+            int width = writeableBitmap.PixelWidth;
+            int height = writeableBitmap.PixelHeight;
+            int stride = width * 4; // 4 байта на пиксель (BGRA)
+
+            byte[] pixels = new byte[height * stride]; // Массив пикселей   
+            writeableBitmap.CopyPixels(pixels, stride, 0);
+
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                byte blue = pixels[i];
+                byte green = pixels[i + 1];
+                byte red = pixels[i + 2];
+
+                byte gray = (byte)(0.3 * red + 0.59 * green + 0.11 * blue); // формула в оттенок серого 
+
+                pixels[i] = gray;
+                pixels[i + 1] = gray;
+                pixels[i + 2] = gray;
+            }
+
+            writeableBitmap.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride, 0);
+            return writeableBitmap;
+        }
+        public static BitmapSource contrastingImage(BitmapSource source)
+        {
+            int stride = (source.PixelWidth * source.Format.BitsPerPixel + 7) / 8;
+            byte[] pixels = new byte[source.PixelHeight * stride];
+            source.CopyPixels(pixels, stride, 0);
+
+            double f_min = 255, f_max = 0;
+
+            for(int i = 0; i < pixels.Length; i += 4)
+            {
+                byte brightness = pixels[i]; // Берём любой канал
+
+                if (brightness < f_min) f_min = brightness;
+                if (brightness > f_max) f_max = brightness;
+            }
+
+            if (f_min == f_max) return source; //проверка на однородность изображения
+
+            double g_min = 0, g_max = 255;
+
+            double a = (g_max - g_min) / (f_max - f_min);
+            double b = (g_min * f_max - g_max * f_min) / (f_max - f_min);
+
+            for(int i = 0; i < pixels.Length; i += 4)
+            {
+                byte brighness = pixels[i]; // Берём любой канал
+                double newbrighness = a * brighness + b; // g = af + b
+                byte newValue = (byte)Math.Max(g_min, Math.Min(g_max, newbrighness));
+
+                pixels[i] = newValue;
+                pixels[i + 1] = newValue;
+                pixels[i + 2] = newValue;
+            }
+
+            return BitmapSource.Create(
+                source.PixelWidth, source.PixelHeight,
+                source.DpiX, source.DpiY,
+                source.Format, null, pixels, stride
+            );
+        }
+        //8 вариант увеличение контрастности
+
+    }
+}
---
+++ ImageProcessingLab4/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace ImageProcessingLab4
+{
+    
+    public partial class MainWindow : Window
+    {
+        private BitmapImage initialImage;
+        private BitmapSource toGrayImage;
+        private BitmapSource contrastImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        public void selectedImage_Click(Object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Изображения|*.png;*.jpg;*.jpeg;*.bmp";
+
+            if(openFileDialog.ShowDialog() == true)
+            {
+                initialImage = new BitmapImage();
+                initialImage.BeginInit();
+                initialImage.UriSource = new Uri(openFileDialog.FileName);
+                initialImage.EndInit();
+
+                selectedImage.Source = initialImage;
+            }
+            
+        }
+        public void transformToGray_Click(Object sender, RoutedEventArgs e)
+        {
+            if(initialImage != null)
+            {
+                toGrayImage = Algoritmic.ConvertRGBToGray(initialImage);
+                toGray.Source = toGrayImage;
+            }
+            else
+            {
+                MessageBox.Show("Изначального изображения нет! Загрузите его");
+            }
+        }
+        public void contrasting_Click(Object sender, RoutedEventArgs e)
+        {
+            if(toGrayImage != null)
+            {
+                contrastImage = Algoritmic.contrastingImage(toGrayImage);
+                contrasting.Source = contrastImage;
+            }
+            else
+            {
+                MessageBox.Show("Нечего контрастировать! Загрузите его");
+            }
+        }
+        public void exitProgram_Click(Object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        
+    }
+
+
+}
